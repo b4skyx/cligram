5b480b278a6f8fd1f96f675ce2e4584d1a03f877
Abhishek Yadav <b4skyx@gmail.com>
2021-10-02 19:50:26 +0530

    repo-init

---
+++ README.md
@@ -0,0 +1,20 @@
+Cligram: A simple grammar quiz written in js using node. Hosted live on [Repl.it](https://replit.com/@AbhishekYadav15/Cligram?embed=1&output=1).
+
+---
+
+### Installation
+
+Clone this repo and install the dependancies with your favourite package manager.
+```
+git clone https://github.com/b4skyx/cligram
+cd cligram
+yarn install
+```
+
+### Modifications
+Edit the data.json file to add or modify the pre-existing questions as per your usecase.
+
+### Sceenshots
+![sample-working](https://user-images.githubusercontent.com/55960554/135720174-aa61c96e-e1d5-4088-9bf4-fa577746d603.png)
+
+![Result](https://user-images.githubusercontent.com/55960554/135720376-0a30fa54-7686-409f-821d-7eecde82729c.png)
---
+++ index.js
@@ -0,0 +1,67 @@
+// Cligram : The Grammar Quiz
+// Author: b4skyx
+// Upstream: https://github.com/b4skyx/cligram
+
+console.log(">>> The Grammar Quiz <<<")
+const readlineSync = require("readline-sync");
+
+const num_questions = 7;
+const data = require("./data.json");
+
+var answered = 0;
+var correct = 0;
+
+// Rnadomize the questions fetched through data and return only the required number
+function get_questions(){
+	var questions = data["questions"];
+	shuffled = questions.sort(() => 0.5 - Math.random());
+	let selected = shuffled.slice(0, num_questions);
+	return selected
+}
+
+// Function to check if the answer is correct or not
+function check_answer(question, answer){
+	if(question["answer"] === answer){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+// Functino to ask question to the user.
+function ask_question(question){
+	var answer;
+	if (question.hasOwnProperty('options')){
+		console.log(question["question"]);
+		index = readlineSync.keyInSelect(question['options'], "Select an option.");
+		answer = question['options'][index];
+	}
+	else{
+		var answer = readlineSync.question(question["question"]+"\nAnswer: ");
+	}
+	if (check_answer(question, answer)){
+		console.log("Correct!");
+		answered++;
+		correct++;
+	}
+	else{
+		console.log("Incorrect. The correct answer is: "+question["answer"])
+		answered++;
+	}
+}
+
+var questions = get_questions();
+// Iterate through the questin list and ask
+for (let index in questions){
+	console.log(`\n=== Question ${parseInt(index)+1} ===\n`)
+	ask_question(questions[index]);
+}
+
+// Print statistics
+console.log("\n========== Stats ==========\n")
+console.log("You secured "+(correct/num_questions*100).toFixed(2)+"%");
+console.log("Total Questions: "+num_questions);
+console.log("Correct: "+correct);
+console.log("Wrong: "+(num_questions-correct))
+console.log("\n===========================\n")
